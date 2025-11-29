ac9fd711302e243814cb20c3124dd8c8e0c79cc1
felix <Wu-Felix@outlook.com>
2025-11-29 20:44:16 +0800

    feat: 初始化日志系统并支持动态配置
    - 为应用配置  日志系统。
    - 日志默认写入每日滚动的日志文件。
    - 可通过  环境变量设置日志级别。
    - 可通过设置  环境变量来启用控制台输出。

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,41 @@
 use color_eyre::Result;
+use tracing_appender::{non_blocking, rolling};
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{
+    EnvFilter,
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // 安装 color_eyre 错误处理
     color_eyre::install()?;
+
+    // -- 文件日志 --
+    let file_appender = rolling::daily("logs", "lazy_net_log");
+    let (non_blocking_writer, _guard) = non_blocking(file_appender);
+    let file_layer = fmt::layer()
+        .with_writer(non_blocking_writer)
+        .with_ansi(false)
+        .with_span_events(FmtSpan::CLOSE);
+
+    // -- 控制台日志 (条件性) --
+    // 检查环境变量 CONSOLE_LOG，如果设置，则启用控制台日志
+    let console_layer = if std::env::var("CONSOLE_LOG").is_err() {
+        None
+    } else {
+        Some(fmt::layer().with_writer(std::io::stdout).with_ansi(true))
+    };
+
+    // 初始化 tracing 订阅器
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("DEBUG")))
+        .with(ErrorLayer::default())
+        .with(file_layer)
+        .with(console_layer)
+        .init();
+
     Ok(())
 }
